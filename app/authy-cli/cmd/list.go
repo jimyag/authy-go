279ec1efdee3e1bf54020869c6ef4929c21f2a3c
jimyag <jiaminyag@gmail.com>
2024-05-26 11:26:55 +0800

    feat: ery beautiful output list results

--- app/authy-cli/cmd/list.go
+++ app/authy-cli/cmd/list.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/spf13/cobra"
 )
 
@@ -22,12 +24,31 @@ func listRun(cmd *cobra.Command, args []string) {
 	if err := client.cfg.Load(client.cfgFile); err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
-
+	t := table.NewWriter()
+	t.Style().Options.DrawBorder = true
+	t.Style().Options.SeparateRows = true
+	tTemp := table.Table{}
+	tTemp.Render()
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{Name: "UniqueId", Align: text.AlignCenter},
+		{Name: "TOTP", Align: text.AlignCenter},
+		{Name: "Lifetime/s", Align: text.AlignCenter},
+		{Name: "Name", Align: text.AlignCenter},
+		{Name: "NextTOTP", Align: text.AlignCenter},
+	})
+	t.AppendHeader(table.Row{"UniqueId", "TOTP", "Lifetime/s", "Name", "NextTOTP"})
 	for _, token := range client.cfg.AuthenticatorTokenS {
-		totp, err := token.TOTP(client.cfg.BackupPassword, time.Now())
+		tm := time.Now()
+		totp, err := token.TOTP(client.cfg.BackupPassword, tm)
 		if err != nil {
 			log.Fatalf("Failed to generate TOTP: %v", err)
 		}
-		_, _ = fmt.Printf("%s: %s \t:%s\n", token.UniqueID, totp, token.Description())
+		lifeTime := 30 - tm.Unix()%30
+		nextTotp, err := token.TOTP(client.cfg.BackupPassword, tm.Add(time.Second*30))
+		if err != nil {
+			log.Fatalf("Failed to generate TOTP: %v", err)
+		}
+		t.AppendRow(table.Row{token.UniqueID, totp, lifeTime, token.Description(), nextTotp})
 	}
+	fmt.Println(t.Render())
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jimyag/authy-go
 go 1.18
 
 require (
+	github.com/jedib0t/go-pretty/v6 v6.5.9
 	github.com/jimyag/version-go v0.1.1
 	github.com/spf13/cobra v1.8.0
 	golang.org/x/crypto v0.23.0
@@ -11,6 +12,8 @@ require (
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 )
